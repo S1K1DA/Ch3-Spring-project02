2b9b3b2ac25330fea75af12e01ba9b63b44bc448
EUNSIK JUNG <bamtol2100@naver.com>
2026-01-08 18:01:02 +0900

    초기 ì 설정

---
+++ src/main/java/com/example/springproject_ch3_01/SpringProjectCh301Application.java
@@ -0,0 +1,13 @@
+package com.example.springproject_ch3_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectCh301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectCh301Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springproject_ch3_01/SpringProjectCh301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject_ch3_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectCh301ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
