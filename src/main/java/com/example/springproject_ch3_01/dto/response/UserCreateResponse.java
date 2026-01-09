3511a013868f061ddfb45677b66680dadeee44c6
EUNSIK JUNG <bamtol2100@naver.com>
2026-01-09 12:51:55 +0900

    fix : git 코드 오류 수정

--- src/main/java/com/example/springproject_ch3_01/dto/response/UserCreateResponse.java
+++ src/main/java/com/example/springproject_ch3_01/dto/response/UserCreateResponse.java
@@ -1,4 +1,28 @@
 package com.example.springproject_ch3_01.dto.response;
 
+import com.example.springproject_ch3_01.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class UserCreateResponse {
+
+    private Long id;
+    private String userName;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static UserCreateResponse from(User user) {
+        return UserCreateResponse.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .email(user.getEmail())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
 }
